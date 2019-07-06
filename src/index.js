252ec9374c1502e440028d2e8fb85eaa075a037f
rinat_gt <pacmanreal@gmail.com>
2019-07-06 17:49:43 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (i = 0; i < bracketsConfig.length; i++) {
+      var reg = bracketsConfig[i].join('');
+      if (str.includes(reg)) {
+          while (str.includes(reg)) {
+              str = str.replace(reg, '');
+          }
+      }
+
+  }
+
+  if (str == '') {
+      return true
+  } else {
+      return false;
+  }
+
 }
